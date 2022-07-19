18896ec7ae4c669be9aecb2899bf22dc517df752
jinnyRyu <hajinryu51@gmail.com>
2022-07-20 00:15:52 +0900

    [ETC] 오타

--- src/test/java/com/example/ftptest/FtpTestApplicationTests.java
+++ src/test/java/com/example/ftptest/FtpTestApplicationTests.java
@@ -48,29 +48,51 @@ class FtpTestApplicationTests {
             String fileData = entry.getFilename().split("[_-]")[1];
 
             FtpData ftpData = new FtpData();
-            ftpData.setAttrDate(KSTtoSDF(attrs.substring(attrs.length()-28)));
+//            ftpData.setAttrDate(KSTtoSDF(attrs.substring(attrs.length()-28))); //문제
             ftpData.setFileName(entry.getFilename());
             ftpData.setFileDate(fileData);
 
             ftpListSort.add(ftpData);
-            log.info("get filename {}",ftpListSort.get(0).getFileName());
         }
+
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+//        Collections.sort(ftpListSort, (o1, o2) -> {
+//            try {
+//                Date o1Time =  sdf.parse(o1.getFileDate());
+//                Date o2Time =  sdf.parse(o2.getFileDate());
+//
+//                if (o1Time.before(o2Time)) {
+//                    return -1;
+//                } else if (o1Time.after(o2Time)) {
+//                    return 1;
+//                }
+//            } catch (ParseException e) {
+//                e.printStackTrace();
+//            }
+//            return 0;
+//        });
+
+//        for (int i =0; i<ftpListSort.size(); i++){
+//
+//            log.info(" listdata {} {}", ftpListSort.get(i).getFileName(),ftpListSort.get(i).getFileDate());
+//        }
 
     }
 
 
-    public String KSTtoSDF(String kst){
-        String strDate = null;
-        try {
-            SimpleDateFormat recvSimpleFormat = new SimpleDateFormat("E MMM dd HH:mm:ss z yyyy", Locale.KOREA);
-
-            SimpleDateFormat tranSimpleFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
-
-            Date data = recvSimpleFormat.parse(kst);
-            strDate = tranSimpleFormat.format(data);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return strDate;
-    }
+//    public String KSTtoSDF(String kst) {
+//
+//        String strDate = null;
+//        try {
+//            SimpleDateFormat recvSimpleFormat = new SimpleDateFormat("E MMM dd HH:mm:ss z yyyy", Locale.KOREA);
+//
+//            SimpleDateFormat tranSimpleFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
+//
+//            Date data = recvSimpleFormat.parse(kst);
+//            strDate = tranSimpleFormat.format(data);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//        return strDate;
+//    }
 }
